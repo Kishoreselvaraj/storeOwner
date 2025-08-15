c425ec6de683ec7b722f29aee12f94560e799d6d
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-08-15 16:56:34 +0530

    bottom nav

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        fontFamily: 'Poppins',
+        fontFamily: 'Inter',
       ),
       debugShowCheckedModeBanner: false,
       home: const AppStack(),
